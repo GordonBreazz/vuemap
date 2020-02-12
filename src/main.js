5c1391860a1f71dc0fbcbdb71eeac0193661664d
GordonBreazz <jc12-mail@yandex.ru>
2020-02-12 16:05:28 +0800

    Added Vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ymapPlugin from 'vue-yandex-maps'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+Vue.use(ymapPlugin)
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
