449e948b656cd2f86881dfc581979b0bb44188a4
GordonBreazz <jc12-mail@yandex.ru>
2020-02-13 17:17:25 +0800

    IN PRocess

--- src/App.vue
+++ src/App.vue
@@ -38,73 +38,20 @@
     </v-app-bar>
 
     <v-content>
-          <yandex-map 
-              :coords="coords" 
-              :zoom="mzoom" 
- 
-              :controls="controls"
-              map-type="map"
-              show-all-markers="true"
-          >
+      <UUMap></UUMap>
 
-      <ymap-marker 
-        marker-id="123" 
-        :coords="coords1"
-        :balloon-template="balloonTemplate"
-        hint-content="Центральная городская библиотека им. И. Калашникова"    
-        :icon="markerIcon"
-        @click="onClick"
-         cluster-name="ИАУ ЦБС г.Улан-Удэ"
-      ></ymap-marker>
-
-      <ymap-marker 
-        marker-id="123" 
-        :coords="coords2"
-        :balloon-template="balloonTemplate"
-        hint-content="Центральная городская библиотека им. И. Калашникова"    
-        :icon="markerIcon"
-        cluster-name="ИАУ ЦБС г.Улан-Удэ"
-      ></ymap-marker>
-
-    </yandex-map>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import UUMap from "./components/UUMap"
 // https://codesandbox.io/s/codesandbox-nuxt-1ht4k
 export default {
-  data: () => ({
-    coords: [ 51.834464, 107.584574],
-    coords1: [51.825683, 107.58439],
-    coords2: [51.74677212790084, 107.6959137288361],
-    mzoom: 12,
-    controls: ["zoomControl"],
-    markerIcon: {
-      layout: 'default#imageWithContent',
-      imageHref: 'http://cbs-uu.ru//images/assets/pins-maps-library.png',
-      imageSize: [50, 50],
-      imageOffset: [0, 0],
-      content: 'Центральная городская библиотека им. И. Калашникова',
-      contentOffset: [-25, 55],
-      contentLayout: '<div style="border-radius: 8px; background-color: rgba(117, 190, 218, 0.5);  width: 100px; color: red; font-weight: bold;">$[properties.iconContent]</div>'
-    }    
-  }),
-  computed: {
-    balloonTemplate() {
-      return `
-        <h1 class="red">Hi, everyone!</h1>
-        <p>I am here: ${this.coords}</p>
-        <img src="http://via.placeholder.com/350x150">
-      `
-    }
-  },
-  methods: {
-    onClick(e) {
-      //this.coords = e.get('coords');
-    this.mzoom = 15
-    }
+  components: {
+    UUMap
   }  
+ 
 }
 </script>
 
