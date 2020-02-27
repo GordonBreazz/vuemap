a215246e3035ab8a2346a5dbb7e1fb584ae1dc7b
GordonBreazz <jc12-mail@yandex.ru>
2020-02-27 23:41:59 +0800

    Доработал окно балуна

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -56,6 +56,10 @@ export default {
   },
   computed: {},
   methods: {
+    workStatus() {
+      let today = new Date();
+
+    },
     showPanel() {
 //      this.$store.state.showInfoPanel = true;
       this.$store.commit("showInfoPanel");
@@ -83,9 +87,11 @@ export default {
       let buttonSt = "";
       // let info = ''
       // if (location.fullTitle != location.title) info = location.fullTitle
-      if (
+      if (        
+        location.id == 1 ||
         location.id == 2 ||
         location.id == 3 ||
+        location.id == 4 ||
         location.id == 6 ||
         location.id == 9 ||
         location.id == 10 ||
@@ -94,28 +100,33 @@ export default {
         location.id == 15 ||
         location.id == 16 ||
         location.id == 19 ||
+        location.id == 20 ||
         location.id == 21 ||
-        location.id == 24 ||
-        location.id == 20
+        location.id == 24 
       ) {
         // this.panaUrl = `http://cbs-uu.ru/tours/f${location.id}/index.html`
-        // this.$Bus.$emit('my-sample-event', this.panaUrl);
+        // this.$Bus.$emit('my-sample-event', this.panaUrl);        
         frameSt =
-          '<div style="margin-bottom:10px;"><iframe src="http://cbs-uu.ru/tours/f' +
+          '<div style="margin-bottom:0px;"><div style="width: 400px; height: 250px;"><iframe src="http://cbs-uu.ru/tours/f' +
           location.id +
-          '/index.html" width="400" height="250" align="left">Ваш браузер не поддерживает плавающие фреймы!</iframe></div><div style="height: 240px;"></div>';
+          '/index.html" width="100%" height="100%" align="left">Ваш браузер не поддерживает плавающие фреймы!</iframe></div></div>';
         buttonSt = `
-          <button  type="button" class="v-btn v-btn--absolute v-btn--contained v-btn--fab v-btn--right v-btn--round v-btn--top theme--dark v-size--small red accent-3 ml-3" style="margin-top:313px" onclick="javascript:document.querySelector('#button100700').click()">
+          <button  type="button" class="v-btn v-btn--absolute v-btn--contained v-btn--fab v-btn--right v-btn--round v-btn--top theme--dark v-size--small red accent-3 ml-3" style="margin-top:332px" onclick="javascript:document.querySelector('#button100700').click()">
             <span class="v-btn__content">
               <i aria-hidden="true" class="v-icon notranslate mdi mdi-arrow-expand-all theme--dark "></i>
             </span>
           </button>`;
       } else if (location.photoSmall)
         frameSt = '<img style="width: 100%" src="' + location.photoSmall + '">';//'<div role="progressbar" aria-valuemin="0" aria-valuemax="100" class="v-progress-circular v-progress-circular--indeterminate purple--text" style="height: 70px; width: 70px;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="22.22222222222222 22.22222222222222 44.44444444444444 44.44444444444444" style="transform: rotate(0deg);"><circle fill="transparent" cx="44.44444444444444" cy="44.44444444444444" r="20" stroke-width="4.444444444444445" stroke-dasharray="125.664" stroke-dashoffset="125.66370614359172px" class="v-progress-circular__overlay"></circle></svg><div class="v-progress-circular__info"></div></div>'//
+
+      let today = new Date();
+      let stTime = today.getUTCHours() + 8 + ":" + today.getUTCMinutes() + ":" + today.getUTCSeconds();
+
       return `
-        <h1>${location.fullTitle}</h1>
-        <p style="margin-top: 10px; margin-bottom: 5px;"><i>Адрес библиотеки:
-        <b>${location.address}</b></i></p>
+        <h2 style="margin-top: 0px;">${location.fullTitle}</h2>
+        <p style="margin-top: 8px; margin-bottom: 4px;"><i>Адрес:
+        <b>${location.address}</b>
+        <br><b>Сейчас закрыто.</b> До открытия библиотеки 11 часов.${stTime}</i></p>
         ${frameSt}<div style="height: 10px;"></div>
         <button type="button" class="v-btn v-btn--contained  v-size--default  primary mb-1" onclick="javascript: document.querySelector('#button100500').click();">Подробнее</button> 
         <button type="button" class="v-btn v-btn--contained  v-size--default  light-blue white--text mb-1" onclick="javascript: document.querySelector('#button100600').click();">ВСЕ БИБЛИОТЕКИ </button>
