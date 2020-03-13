a7ec7afd89622c13165e0032c95e2947696a0d1e
GordonBreazz <jc12-mail@yandex.ru>
2020-03-13 13:54:13 +0800

    Сделал превью фото

--- src/components/DetailPanel.vue
+++ src/components/DetailPanel.vue
@@ -24,10 +24,12 @@
               <v-row>
                 <v-col cols="10">
                   <v-row>
-                    <v-col v-for="(item, index) in this.$store.getters.getFeaturesImages" :key="index">
+                    <v-col
+                      v-for="(item, index) in this.$store.getters.getFeaturesImages"
+                      :key="index"
+                    >
                       <v-img :src="item" width="40"></v-img>
                     </v-col>
-
                   </v-row>
                 </v-col>
                 <v-col cols="2">
@@ -37,7 +39,34 @@
               <div
                 style="text-align: justify; color: #454545;"
                 v-html="this.$store.state.currentLocation.text"
-              ></div>
+              >
+              
+              
+              
+              </div>
+                    <v-container>
+                      <v-row no-gutters>
+                        <v-col cols="5"> 
+                          <v-img src="https://picsum.photos/510/300?random" class="mr-1"></v-img>
+                        </v-col>
+                        <v-col cols="5">
+                          <v-img src="https://picsum.photos/510/300?random" class="mr-1"></v-img>
+                        </v-col>
+                        <v-col cols="2">
+                          <v-img
+                            src="https://picsum.photos/510/300?random"
+                            width="200"
+                            height="100%"
+                          
+                          ><div style="background-color:rgba(51,51,51,.8); height: 100%; color: white; justify-content: center;   position: relative ;  align-items: center; text-align: center; ">
+                            <p style="position: absolute; top: 45%; width: 100%">6 фото</p>
+                            </div></v-img>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+
+
+
             </v-col>
             <v-col cols="12" md="6">
               <v-container>
@@ -46,40 +75,19 @@
                     <FeaturesList />
                   </v-col>
                   <v-col cols="12" lg="6">
-                    <ContactDetailsList />
+                    <ContactDetailsList />                    
                   </v-col>
                 </v-row>
               </v-container>
             </v-col>
           </v-row>
         </v-container>
-        <v-container>
-          <v-row>
-            <v-col >
-         <v-img src="https://picsum.photos/510/300?random" ></v-img>
-              </v-col>
-            <v-col >
-         <v-img src="https://picsum.photos/510/300?random" ></v-img>
-              </v-col>
-            <v-col >
-                
-          
-          
-        >
-         <v-img src="https://picsum.photos/510/300?random" width="200" height="100%" style></v-img>
-              </v-col>
-              </v-row>
-              </v-container>
-
       </v-card-text>
       <v-divider></v-divider>
       <v-card-actions>
         <v-btn @click="showPanViewer" large color="pink" dark outlined>
           <v-icon dark class="mr-2">mdi-arrow-decision-outline</v-icon>3D тур по библиотеке
         </v-btn>
-
-
-                
       </v-card-actions>
     </v-card>
   </div>
@@ -117,5 +125,36 @@ export default {
 </script>
 
 <style>
+#more-photo {
+  -ms-flex-align: center;
+      align-items: center;
+  background: rgba(51, 51, 51, 0.8);
+  color: white;
+  cursor: pointer;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-pack: center;
+      justify-content: center;
+  position: relative;
+  width: 20%;
+    opacity: 0.1;
+}
+
+
+</style>
+
+<style scoped>
+  .bottom-gradient {
+    background-image: linear-gradient(to top, rgba(0, 0, 0, 0.4) 0%, transparent 72px);
+  }
+
+  .repeating-gradient {
+    background-image: repeating-linear-gradient(-45deg,
+                        rgba(255,0,0,.25),
+                        rgba(255,0,0,.25) 5px,
+                        rgba(0,0,255,.25) 5px,
+                        rgba(0,0,255,.25) 10px
+                      );
+  }
 </style>
 
