fabf3c60cde42805b4107a26d1412314d637f9fa
GordonBreazz <jc12-mail@yandex.ru>
2020-04-21 23:51:43 +0800

    http response code

--- src/PHP/test.php
+++ src/PHP/test.php
@@ -1,3 +1,17 @@
 <?
 echo "test"
+if (!function_exists('http_response_code'))
+{
+    function http_response_code($newcode = NULL)
+    {
+        static $code = 200;
+        if($newcode !== NULL)
+        {
+            header('X-PHP-Response-Code: '.$newcode, true, $newcode);
+            if(!headers_sent())
+                $code = $newcode;
+        }       
+        return $code;
+    }
+}
 ?>
