1b2db9e6f5b2b708290bf25889d49b4e59cd6b99
GordonBreazz <jc12-mail@yandex.ru>
2020-05-23 23:55:23 +0800

    fix

--- src/components/AfficheEvents/FullscreenView.vue
+++ src/components/AfficheEvents/FullscreenView.vue
@@ -24,6 +24,16 @@ e to limited space, full-screen dialogs may be more appropriate for mobile devic
       </v-fab-transition>
       <h1>1211212</h1>
       {{eventdata.imagePath}}
+      <div>
+    <p class="text-left">Left aligned text on all viewport sizes.</p>
+    <p class="text-center">Center aligned text on all viewport sizes.</p>
+    <p class="text-right">Right aligned text on all viewport sizes.</p>
+
+    <p class="text-sm-left">Left aligned text on viewports sized SM (small) or wider.</p>
+    <p class="text-md-left">Left aligned text on viewports sized MD (medium) or wider.</p>
+    <p class="text-lg-left">Left aligned text on viewports sized LG (large) or wider.</p>
+    <p class="text-xl-left">Left aligned text on viewports sized XL (extra-large) or wider.</p>
+  </div>
     </div>
   </v-dialog>
 </template>
