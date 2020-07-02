2ad0ef00571200a36a0b056cb02e37180dd865e5
GordonBreazz <jc12-mail@yandex.ru>
2020-07-03 00:27:44 +0800

    Лайаут для окна логина

--- src/layout/AppContent.vue
+++ src/layout/AppContent.vue
@@ -1,8 +1,10 @@
 <template>
-  <v-content class="grey lighten-5">
-    <v-container class="white" style="box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+
+  <v-content class="grey lighten-5" >
+    <v-container class="white" style="box-shadow: 0 0 10px rgba(0,0,0,0.1);" v-if="($route.name != 'SingIn')">
       <router-view></router-view>
     </v-container>
+    <router-view v-else></router-view>
   </v-content>
 </template>
 
