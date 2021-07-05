b6c3037853f8551f5d9c72cb988f435108823b34
GordonBreazz <jc12-mail@yandex.ru>
2021-07-05 23:53:51 +0800

    fix

--- src/store/modules/CultureEvents.js
+++ src/store/modules/CultureEvents.js
@@ -1,5 +1,5 @@
 //const URLAPI = "http://cbs-uu.ru/data/json_proxy.php?status=accepted&start=1588291200&organizations=10872&sort=-start&apiKey=" + process.env.VUE_APP_ALLCULTURE_APIKEY
-//const URLAPI = "https://all.culture.ru/api/2.5/events?status=accepted&start=1588291200&organizations=10872&sort=-start&apiKey=" + process.env.VUE_APP_ALLCULTURE_APIKEY
+const URLAPI = "https://all.culture.ru/api/2.5/events?status=accepted&start=1588291200&organizations=10872&sort=-start&apiKey=" + process.env.VUE_APP_ALLCULTURE_APIKEY
 const IMAGESPATH = "https://all.culture.ru/uploads/"
 
 function titlePart1(value) {
